8aa80a49016a133fe1acf220b2b6d3442a6b147d
Thakur <namit1903@gmail.com>
2025-01-23 18:23:02 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Link to="/" >About</Link>
       <Link to="/">Contact</Link>
     </div>
-    <p className='text-sm'>Created By Dynamic Coding with Amit</p>
+    <p className='text-sm'>A practice Project By Namit Thakur 2024</p>
 </footer>
   )
 }
